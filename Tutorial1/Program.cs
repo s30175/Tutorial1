d93ae504852383712287ffda4b8f85f38c20373d
s30175 <s30175@pjwstk.edu.pl>
2025-03-03 18:33:12 +0100

    Calculate average feature

--- Tutorial1/Program.cs
+++ Tutorial1/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+static double CalculateAverage(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine(CalculateAverage(arr));
