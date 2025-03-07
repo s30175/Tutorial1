bae095c7fc30417833e428847f5b6f195a9565eb
s30175 <s30175@pjwstk.edu.pl>
2025-03-07 16:06:59 +0100

    Created FindMaximum() function

--- Tutorial1/Program.cs
+++ Tutorial1/Program.cs
@@ -18,3 +18,17 @@ static double CalculateAverage(int[] arr)
 
 int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
 Console.WriteLine(CalculateAverage(arr));
+
+static int FindMaximum(int[] arr)
+{
+    int maximum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maximum) maximum = arr[i];
+    }
+
+    return maximum;
+}
+
+Console.WriteLine(FindMaximum(arr));
